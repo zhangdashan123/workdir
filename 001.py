f37222f86486c46af17b4fdf04b8ed8fc1041ae9
zhangdashan123 <39666864+zhangdashan123@users.noreply.github.com>
2019-03-27 13:26:20 +0800

    第四版v2.0
    Signed-off-by: zhangyadong <zhangyadong@xywy.com>

--- 001.py
+++ 001.py
@@ -5,4 +5,4 @@ import re
 str = '我的个人邮箱是cqc@cuiqingcai.com，个人博客是cuiqingcai.com，个人公众号是进击的Coder'
 result = re.search('(?<=，)(?<!。)个人博客是(.*?)(?=，)', str)
 print('整句结果：' + result.group(), '第一个匹配结果：' + result.group(1), sep='\n')
-'''这是第二版'''
+'''这是第四版'''
