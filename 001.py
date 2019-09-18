23a42b866fb60db4be634d644ad12f41a75dc317
zhangdashan123 <276333903@qq.com>
2019-09-18 22:25:40 +0800

    0918最新

--- 001.py
+++ 001.py
@@ -5,4 +5,5 @@ import re
 str = '我的个人邮箱是cqc@cuiqingcai.com，个人博客是cuiqingcai.com，个人公众号是进击的Coder'
 result = re.search('(?<=，)(?<!。)个人博客是(.*?)(?=，)', str)
 print('整句结果：' + result.group(), '第一个匹配结果：' + result.group(1), sep='\n')
-'''这是第6版'''
+'''这是第8版'''
+print('haha')
